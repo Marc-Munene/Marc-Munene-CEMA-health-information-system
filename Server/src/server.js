bfe5a3eaff5103e0cce0b2f42469b8e840f9b4a3
Marc Munene <marcmunene104@gmail.com>
2025-04-24 21:51:11 +0300

    CRUD routes for doctors, clients, and programs

--- Server/src/server.js
+++ Server/src/server.js
@@ -2,8 +2,65 @@ import express from "express";
 
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
+
+// Home route
+app.get("/", (req, res) => {
+  res.send("Home Route");
+});
+
+// Doctors route
+app.get("/doc", (req, res) => {
+  res.send("Getting Doctors");
+});
+
+app.post("/doc", (req, res) => {
+  res.send("Adding Doctors");
+});
+
+app.put("/doc", (req, res) => {
+  res.send("Updating Doctors");
+});
+
+app.delete("/doc", (req, res) => {
+  res.send("Deleting Doctors");
+});
+
+//Clients route
+app.get("/client", (req, res) => {
+  res.send("Getting Clients");
+});
+
+app.post("/client", (req, res) => {
+  res.send("Adding Clients");
+});
+
+app.put("/client", (req, res) => {
+  res.send("Updating Clients");
+});
+
+app.delete("/client", (req, res) => {
+  res.send("Deleting Clients");
+});
+
+//Programs route
+app.get("/program", (req, res) => {
+  res.send("Getting Programs");
+});
+
+app.post("/program", (req, res) => {
+  res.send("Adding Programs");
+});
+
+app.put("/programs", (req, res) => {
+  res.send("Updating Programs");
+});
+
+app.delete("/program", (req, res) => {
+  res.send("Deleting Programs");
+});
+
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+  console.log(`Server is running on port ${PORT}`);
+});
