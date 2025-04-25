962332f9977fd0bfeb1299522b99d95561b72e73
Marc Munene <marcmunene104@gmail.com>
2025-04-25 17:15:51 +0300

    routes

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,11 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { App } from './App'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { App } from "./App";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { GlobalLayout } from "./pages/protected/GlobalLayout";
+import { Client } from "./pages/protected/Client";
+import { Programs } from "./pages/protected/Programs";
+import { Enrollment } from "./pages/protected/Enrollment";
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route element={<GlobalLayout />}>
+          <Route path="/" element={<App />} />
+          <Route index element={<Client />} />
+          <Route path="/programs" element={<Programs />} />
+          <Route path="/enrollments" element={<Enrollment />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
