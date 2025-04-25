e265102627a0b7b71a6b083e4967c901e87f550d
Marc Munene <marcmunene104@gmail.com>
2025-04-25 17:05:05 +0300

    pages

---
+++ Client/src/pages/protected/Programs.jsx
@@ -0,0 +1,13 @@
+const Programs = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Programs</h1>
+      <p className="mt-4">This is the Programs page.</p>
+      <p className="mt-4">You can manage your programs here.</p>
+      <p className="mt-4">You can add, edit, and delete programs.</p>
+      <p className="mt-4">You can also view the details of each program.</p>
+    </div>
+  );
+};
+
+export { Programs };
